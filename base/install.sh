27f3e3c889b0104389ee9be6e498b0c829638fb4
afokepeniel <afokekowho@gmail.com>
2024-04-29 23:13:47 -0400

    Added folders for RHCE study

---
+++ base/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ansible all -m yum -a "name=httpd state=present"
+ansible all -m service -a "name=httpd state=started enabled=yes"
+ansible all -m user -a "name=anna state=present"
+ansible all -m copy -a "src=/etc/hosts dest=/temp/"
